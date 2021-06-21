b1a9c626b38aa962295325f11d920bc7745301af
skrillee <1979736774@qq.com>
2021-06-21 19:32:07 +0800

    修改url中的ProSearchOrderUsernamePricesView

--- api/urls.py
+++ api/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^login/', views.LoginView.as_view()),
     # url(r'^freight_bill/', views.FreightBill.as_view()),
     url(r'^pro/login/', views.ProLoginView.as_view()),
-    url(r'^pro/order/', views.ProOrderView.as_view()),
+    url(r'^pro/order/', views.ProSearchOrderUsernamePricesView.as_view()),
 ]
