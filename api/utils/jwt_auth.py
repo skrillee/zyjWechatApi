0ed2424fcacf8f41dc570aceafc8bba07a189b64
skrillee <1979736774@qq.com>
2021-06-22 14:43:33 +0800

    修改tolken失效时间，timeout改为五周后失效

--- api/utils/jwt_auth.py
+++ api/utils/jwt_auth.py
@@ -3,13 +3,13 @@ import datetime
 from django.conf import settings
 
 
-def create_token(payload, timeout=1):
+def create_token(payload, timeout=5):
 
     salt = settings.SECRET_KEY
     headers = {
         'typ': 'jwt',
         'alg': 'HS256'
     }
-    payload['exp'] = datetime.datetime.utcnow() + datetime.timedelta(minutes=timeout)
+    payload['exp'] = datetime.datetime.utcnow() + datetime.timedelta(weeks=timeout)
     token = jwt.encode(payload=payload, key=salt, algorithm="HS256", headers=headers)
     return token.decode()
