735b9eea7bee51cabac66d1672b8501fefba931f
skrillee <1979736774@qq.com>
2021-08-12 13:36:46 +0800

    修改jwt登陆时间，从五个月改为8小时

--- api/utils/jwt_auth.py
+++ api/utils/jwt_auth.py
@@ -3,13 +3,13 @@ import datetime
 from django.conf import settings
 
 
-def create_token(payload, timeout=5):
+def create_token(payload, timeout=8):
 
     salt = settings.SECRET_KEY
     headers = {
         'typ': 'jwt',
         'alg': 'HS256'
     }
-    payload['exp'] = datetime.datetime.utcnow() + datetime.timedelta(weeks=timeout)
+    payload['exp'] = datetime.datetime.utcnow() + datetime.timedelta(hours=timeout)
     token = jwt.encode(payload=payload, key=salt, algorithm="HS256", headers=headers)
-    return token.decode()
+    return token
